85b1a77af0bf98267b2324353d2159ca05dc5189
root <root@DESKTOP-B19RQ5P>
2024-08-18 17:01:00 +0300

    task6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Annotated Function to return sum of list."""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[int, float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Return Function that sum list."""
     sum = 0
     for i in range(0, len(mxd_list)):
