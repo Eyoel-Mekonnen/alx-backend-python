76379446d2cd962f30e76703f32457e5a5e95028
root <root@DESKTOP-B19RQ5P>
2024-08-18 14:56:06 +0300

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type Annotated Function that takes string and return string."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return concatenated strings."""
+    return str1 + str2
