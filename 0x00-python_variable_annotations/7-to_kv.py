05a9100f21479ae88d1143f428d50d75f141c76f
root <root@DESKTOP-B19RQ5P>
2024-08-18 17:29:58 +0300

    task7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+"""Function that take str and int | float and return tuple."""
 from typing import Tuple, Union
-"""Function that take str and int | float and return tuple"""
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return newTuple"""
+    """Return newTuple."""
     v = float(v**2)
     newTuple = (k, v)
     return newTuple
