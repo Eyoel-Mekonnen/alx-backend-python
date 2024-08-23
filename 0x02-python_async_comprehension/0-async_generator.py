bf8c943195787f43a574605a3840839669b2a0a3
root <root@DESKTOP-3SEIDIF>
2024-08-23 18:24:15 +0300

    task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Yield number on async loop."""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Return Yeild random number."""
     for i in range(0, 10):
         await asyncio.sleep(1)
