944a28bc390c3ab0e627cf24cc5acea221142be7
root <root@DESKTOP-3SEIDIF>
2024-08-23 19:32:12 +0300

    task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """Async Comprehension."""
 from typing import Generator
 import asyncio
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Return 10 random number using __aiter__()."""
     results = [i async for i in async_generator()]
     return results
