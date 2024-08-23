97f463bf0fac5459bb303ca3aa933ba632ac49a5
root <root@DESKTOP-3SEIDIF>
2024-08-23 19:57:57 +0300

    task2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Return the measured Time."""
     start = time.time()
     await asyncio.gather(*[async_comprehension() for i in range(0, 4)])
