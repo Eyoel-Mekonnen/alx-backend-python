6e3fe3fc17efffc19fb474fa92febba3639aceba
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-06-01 21:19:29 +0300

    log

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sqlite3
+import functools
+
+# Decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Attempt to extract the SQL query from arguments
+        query = kwargs.get('query')
+        if query is None and len(args) > 0:
+            query = args[0]
+        print(f"Executing SQL query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+# Fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+
