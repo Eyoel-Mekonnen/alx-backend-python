1f8cf5f44b884238859c621b88afe6982de5dee3
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-06-08 22:35:45 +0300

    view

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,35 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets, status
+from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']  # Change 'title' if your model uses a different field
 
     def create(self, request, *args, **kwargs):
-        # Handle creation of a new conversation
-        return super().create(request, *args, **kwargs)
-
-    @action(detail=True, methods=['post'])
-    def send_message(self, request, pk=None):
-        # Send a message to an existing conversation (pk = conversation id)
-        conversation = self.get_object()
-        serializer = MessageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(conversation=conversation, sender=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']  # Change 'content' if your model uses a different field
 
     def create(self, request, *args, **kwargs):
-        # Optionally override if needed
-        return super().create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
